b1fb6805076bc0784f8842a7f98b58df85389941
Linda McGraw <linda.mcgraw@me.com>
2019-09-24 15:20:02 -0500

    correcting

--- test/test_rsvps.php
+++ test/test_rsvps.php
@@ -10,7 +10,7 @@ require(base_path . "/model/RSVPs.php");
 
 $rsvp = new RSVPs(1);
 var_dump($rsvp->read(1)) . PHP_EOL;
-$rsvp->set_value("number_in_party", "1");
+$rsvp->set_value("number_in_party", "2");
 $rsvp->update( $rsvp->get_rsvp_id());
 var_dump($rsvp->get_all(1));
 $rsvp->delete();
