cee728a294db44a6e7425ab841db63ffc08ca6ce
Linda McGraw <linda.mcgraw@me.com>
2019-09-25 14:57:48 -0500

    dashboard nav add couple dropdown

--- test/test_rsvps.php
+++ test/test_rsvps.php
@@ -11,22 +11,25 @@ require(base_path . "/model/RSVPs.php");
 $rsvp = new RSVPs();
 //var_dump($rsvp->get_all(1));
 
-$rsvp->set_value("event_id", 1);
-$rsvp->set_value("first_name", "Pam");
+$rsvp->set_value("event_id", 2);
+$rsvp->set_value("first_name", "Becky");
 $rsvp->set_value("last_name", "Example");
-$rsvp->set_value("email", "Pam@example.com");
+$rsvp->set_value("email", "Becky@example.com");
 $rsvp->set_value("events", "Reception");
 $rsvp->set_value("status", "Not Going");
-$rsvp->set_value("number_in_party", "1");
+$rsvp->set_value("number_in_party", "2");
 $rsvp->set_value("create_date", "CURRENT_TIMESTAMP");
 $rsvp->set_value("last_update_date", "CURRENT_TIMESTAMP");
-$rsvp->create();
-
+echo $rsvp->create() . PHP_EOL;
+/*
+//test updating number in party
 $rsvp = new RSVPs(1);
 var_dump($rsvp->read(1)) . PHP_EOL;
 $rsvp->set_value("number_in_party", "4");
 $rsvp->update( $rsvp->get_rsvp_id());
 var_dump($rsvp->get_all(1));
+
+*/
 $rsvp->delete();
 
 
